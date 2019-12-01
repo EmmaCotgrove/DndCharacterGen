670d3e80ccbecbb424ec98931cb36323fa5b085d
Emma Cotgrove <emmacotgrove@gmail.com>
2019-12-01 21:42:20 +0000

    modifiers added

--- DnDCharacterGen/BasicCreateForm.Designer.cs
+++ DnDCharacterGen/BasicCreateForm.Designer.cs
@@ -47,19 +47,25 @@
             this.numericUpDown8 = new System.Windows.Forms.NumericUpDown();
             this.label10 = new System.Windows.Forms.Label();
             this.ClassLabel = new System.Windows.Forms.Label();
-            this.StrLabel = new System.Windows.Forms.Label();
             this.label11 = new System.Windows.Forms.Label();
             this.HitDiceBox = new System.Windows.Forms.TextBox();
-            this.DexLabel = new System.Windows.Forms.Label();
-            this.ConsLabel = new System.Windows.Forms.Label();
-            this.WisLabel = new System.Windows.Forms.Label();
-            this.IntLabel = new System.Windows.Forms.Label();
-            this.ChaLabel = new System.Windows.Forms.Label();
             this.ACLabel = new System.Windows.Forms.Label();
             this.HPLabel = new System.Windows.Forms.Label();
             this.otherInfoLbl = new System.Windows.Forms.TextBox();
             this.button1 = new System.Windows.Forms.Button();
             this.RaceLabel = new System.Windows.Forms.Label();
+            this.strMod = new System.Windows.Forms.Label();
+            this.dexMod = new System.Windows.Forms.Label();
+            this.conMod = new System.Windows.Forms.Label();
+            this.wisMod = new System.Windows.Forms.Label();
+            this.intMod = new System.Windows.Forms.Label();
+            this.chaMod = new System.Windows.Forms.Label();
+            this.StrLabel = new System.Windows.Forms.Label();
+            this.DexLabel = new System.Windows.Forms.Label();
+            this.ConLabel = new System.Windows.Forms.Label();
+            this.WisLabel = new System.Windows.Forms.Label();
+            this.IntLabel = new System.Windows.Forms.Label();
+            this.ChaLabel = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
@@ -149,6 +155,7 @@
             this.numericUpDown1.Name = "numericUpDown1";
             this.numericUpDown1.Size = new System.Drawing.Size(79, 22);
             this.numericUpDown1.TabIndex = 8;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.strMod_Click);
             // 
             // numericUpDown2
             // 
@@ -156,6 +163,7 @@
             this.numericUpDown2.Name = "numericUpDown2";
             this.numericUpDown2.Size = new System.Drawing.Size(79, 22);
             this.numericUpDown2.TabIndex = 9;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
             // 
             // numericUpDown3
             // 
@@ -163,6 +171,7 @@
             this.numericUpDown3.Name = "numericUpDown3";
             this.numericUpDown3.Size = new System.Drawing.Size(79, 22);
             this.numericUpDown3.TabIndex = 10;
+            this.numericUpDown3.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
             // 
             // numericUpDown4
             // 
@@ -170,6 +179,7 @@
             this.numericUpDown4.Name = "numericUpDown4";
             this.numericUpDown4.Size = new System.Drawing.Size(79, 22);
             this.numericUpDown4.TabIndex = 11;
+            this.numericUpDown4.ValueChanged += new System.EventHandler(this.numericUpDown4_ValueChanged);
             // 
             // numericUpDown5
             // 
@@ -177,6 +187,7 @@
             this.numericUpDown5.Name = "numericUpDown5";
             this.numericUpDown5.Size = new System.Drawing.Size(79, 22);
             this.numericUpDown5.TabIndex = 12;
+            this.numericUpDown5.ValueChanged += new System.EventHandler(this.numericUpDown5_ValueChanged);
             // 
             // numericUpDown6
             // 
@@ -184,10 +195,11 @@
             this.numericUpDown6.Name = "numericUpDown6";
             this.numericUpDown6.Size = new System.Drawing.Size(79, 22);
             this.numericUpDown6.TabIndex = 13;
+            this.numericUpDown6.ValueChanged += new System.EventHandler(this.numericUpDown6_ValueChanged);
             // 
             // numericUpDown7
             // 
-            this.numericUpDown7.Location = new System.Drawing.Point(447, 116);
+            this.numericUpDown7.Location = new System.Drawing.Point(596, 116);
             this.numericUpDown7.Name = "numericUpDown7";
             this.numericUpDown7.Size = new System.Drawing.Size(79, 22);
             this.numericUpDown7.TabIndex = 15;
@@ -195,7 +207,7 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(379, 118);
+            this.label9.Location = new System.Drawing.Point(528, 118);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(26, 17);
             this.label9.TabIndex = 14;
@@ -203,7 +215,7 @@
             // 
             // numericUpDown8
             // 
-            this.numericUpDown8.Location = new System.Drawing.Point(447, 143);
+            this.numericUpDown8.Location = new System.Drawing.Point(596, 143);
             this.numericUpDown8.Name = "numericUpDown8";
             this.numericUpDown8.Size = new System.Drawing.Size(79, 22);
             this.numericUpDown8.TabIndex = 17;
@@ -211,7 +223,7 @@
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(379, 145);
+            this.label10.Location = new System.Drawing.Point(528, 145);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(68, 17);
             this.label10.TabIndex = 16;
@@ -227,18 +239,10 @@
             this.ClassLabel.TabIndex = 18;
             this.ClassLabel.Text = "label11";
             // 
-            // StrLabel
-            // 
-            this.StrLabel.AutoSize = true;
-            this.StrLabel.Location = new System.Drawing.Point(252, 121);
-            this.StrLabel.Name = "StrLabel";
-            this.StrLabel.Size = new System.Drawing.Size(0, 17);
-            this.StrLabel.TabIndex = 19;
-            // 
             // label11
             // 
             this.label11.AutoSize = true;
-            this.label11.Location = new System.Drawing.Point(379, 175);
+            this.label11.Location = new System.Drawing.Point(528, 175);
             this.label11.Name = "label11";
             this.label11.Size = new System.Drawing.Size(57, 17);
             this.label11.TabIndex = 20;
@@ -246,55 +250,15 @@
             // 
             // HitDiceBox
             // 
-            this.HitDiceBox.Location = new System.Drawing.Point(443, 175);
+            this.HitDiceBox.Location = new System.Drawing.Point(592, 175);
             this.HitDiceBox.Name = "HitDiceBox";
             this.HitDiceBox.Size = new System.Drawing.Size(100, 22);
             this.HitDiceBox.TabIndex = 21;
             // 
-            // DexLabel
-            // 
-            this.DexLabel.AutoSize = true;
-            this.DexLabel.Location = new System.Drawing.Point(252, 150);
-            this.DexLabel.Name = "DexLabel";
-            this.DexLabel.Size = new System.Drawing.Size(0, 17);
-            this.DexLabel.TabIndex = 22;
-            // 
-            // ConsLabel
-            // 
-            this.ConsLabel.AutoSize = true;
-            this.ConsLabel.Location = new System.Drawing.Point(252, 178);
-            this.ConsLabel.Name = "ConsLabel";
-            this.ConsLabel.Size = new System.Drawing.Size(0, 17);
-            this.ConsLabel.TabIndex = 23;
-            // 
-            // WisLabel
-            // 
-            this.WisLabel.AutoSize = true;
-            this.WisLabel.Location = new System.Drawing.Point(252, 195);
-            this.WisLabel.Name = "WisLabel";
-            this.WisLabel.Size = new System.Drawing.Size(0, 17);
-            this.WisLabel.TabIndex = 24;
-            // 
-            // IntLabel
-            // 
-            this.IntLabel.AutoSize = true;
-            this.IntLabel.Location = new System.Drawing.Point(252, 225);
-            this.IntLabel.Name = "IntLabel";
-            this.IntLabel.Size = new System.Drawing.Size(0, 17);
-            this.IntLabel.TabIndex = 25;
-            // 
-            // ChaLabel
-            // 
-            this.ChaLabel.AutoSize = true;
-            this.ChaLabel.Location = new System.Drawing.Point(252, 250);
-            this.ChaLabel.Name = "ChaLabel";
-            this.ChaLabel.Size = new System.Drawing.Size(0, 17);
-            this.ChaLabel.TabIndex = 26;
-            // 
             // ACLabel
             // 
             this.ACLabel.AutoSize = true;
-            this.ACLabel.Location = new System.Drawing.Point(553, 116);
+            this.ACLabel.Location = new System.Drawing.Point(702, 116);
             this.ACLabel.Name = "ACLabel";
             this.ACLabel.Size = new System.Drawing.Size(0, 17);
             this.ACLabel.TabIndex = 27;
@@ -302,7 +266,7 @@
             // HPLabel
             // 
             this.HPLabel.AutoSize = true;
-            this.HPLabel.Location = new System.Drawing.Point(553, 150);
+            this.HPLabel.Location = new System.Drawing.Point(702, 150);
             this.HPLabel.Name = "HPLabel";
             this.HPLabel.Size = new System.Drawing.Size(0, 17);
             this.HPLabel.TabIndex = 28;
@@ -312,7 +276,7 @@
             this.otherInfoLbl.Location = new System.Drawing.Point(54, 320);
             this.otherInfoLbl.Multiline = true;
             this.otherInfoLbl.Name = "otherInfoLbl";
-            this.otherInfoLbl.Size = new System.Drawing.Size(888, 83);
+            this.otherInfoLbl.Size = new System.Drawing.Size(1233, 83);
             this.otherInfoLbl.TabIndex = 29;
             // 
             // button1
@@ -335,24 +299,126 @@
             this.RaceLabel.TabIndex = 31;
             this.RaceLabel.Text = "label12";
             // 
+            // strMod
+            // 
+            this.strMod.AutoSize = true;
+            this.strMod.Location = new System.Drawing.Point(251, 123);
+            this.strMod.Name = "strMod";
+            this.strMod.Size = new System.Drawing.Size(0, 17);
+            this.strMod.TabIndex = 32;
+            // 
+            // dexMod
+            // 
+            this.dexMod.AutoSize = true;
+            this.dexMod.Location = new System.Drawing.Point(251, 150);
+            this.dexMod.Name = "dexMod";
+            this.dexMod.Size = new System.Drawing.Size(0, 17);
+            this.dexMod.TabIndex = 33;
+            // 
+            // conMod
+            // 
+            this.conMod.AutoSize = true;
+            this.conMod.Location = new System.Drawing.Point(251, 178);
+            this.conMod.Name = "conMod";
+            this.conMod.Size = new System.Drawing.Size(0, 17);
+            this.conMod.TabIndex = 34;
+            // 
+            // wisMod
+            // 
+            this.wisMod.AutoSize = true;
+            this.wisMod.Location = new System.Drawing.Point(251, 200);
+            this.wisMod.Name = "wisMod";
+            this.wisMod.Size = new System.Drawing.Size(0, 17);
+            this.wisMod.TabIndex = 35;
+            // 
+            // intMod
+            // 
+            this.intMod.AutoSize = true;
+            this.intMod.Location = new System.Drawing.Point(251, 225);
+            this.intMod.Name = "intMod";
+            this.intMod.Size = new System.Drawing.Size(0, 17);
+            this.intMod.TabIndex = 36;
+            // 
+            // chaMod
+            // 
+            this.chaMod.AutoSize = true;
+            this.chaMod.Location = new System.Drawing.Point(251, 250);
+            this.chaMod.Name = "chaMod";
+            this.chaMod.Size = new System.Drawing.Size(0, 17);
+            this.chaMod.TabIndex = 37;
+            // 
+            // StrLabel
+            // 
+            this.StrLabel.AutoSize = true;
+            this.StrLabel.Location = new System.Drawing.Point(287, 122);
+            this.StrLabel.Name = "StrLabel";
+            this.StrLabel.Size = new System.Drawing.Size(0, 17);
+            this.StrLabel.TabIndex = 38;
+            // 
+            // DexLabel
+            // 
+            this.DexLabel.AutoSize = true;
+            this.DexLabel.Location = new System.Drawing.Point(287, 147);
+            this.DexLabel.Name = "DexLabel";
+            this.DexLabel.Size = new System.Drawing.Size(0, 17);
+            this.DexLabel.TabIndex = 39;
+            // 
+            // ConLabel
+            // 
+            this.ConLabel.AutoSize = true;
+            this.ConLabel.Location = new System.Drawing.Point(287, 173);
+            this.ConLabel.Name = "ConLabel";
+            this.ConLabel.Size = new System.Drawing.Size(0, 17);
+            this.ConLabel.TabIndex = 40;
+            // 
+            // WisLabel
+            // 
+            this.WisLabel.AutoSize = true;
+            this.WisLabel.Location = new System.Drawing.Point(287, 198);
+            this.WisLabel.Name = "WisLabel";
+            this.WisLabel.Size = new System.Drawing.Size(0, 17);
+            this.WisLabel.TabIndex = 41;
+            // 
+            // IntLabel
+            // 
+            this.IntLabel.AutoSize = true;
+            this.IntLabel.Location = new System.Drawing.Point(287, 223);
+            this.IntLabel.Name = "IntLabel";
+            this.IntLabel.Size = new System.Drawing.Size(0, 17);
+            this.IntLabel.TabIndex = 42;
+            // 
+            // ChaLabel
+            // 
+            this.ChaLabel.AutoSize = true;
+            this.ChaLabel.Location = new System.Drawing.Point(287, 248);
+            this.ChaLabel.Name = "ChaLabel";
+            this.ChaLabel.Size = new System.Drawing.Size(0, 17);
+            this.ChaLabel.TabIndex = 43;
+            // 
             // BasicCreateForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1043, 450);
+            this.ClientSize = new System.Drawing.Size(1324, 450);
+            this.Controls.Add(this.ChaLabel);
+            this.Controls.Add(this.IntLabel);
+            this.Controls.Add(this.WisLabel);
+            this.Controls.Add(this.ConLabel);
+            this.Controls.Add(this.DexLabel);
+            this.Controls.Add(this.StrLabel);
+            this.Controls.Add(this.chaMod);
+            this.Controls.Add(this.intMod);
+            this.Controls.Add(this.wisMod);
+            this.Controls.Add(this.conMod);
+            this.Controls.Add(this.dexMod);
+            this.Controls.Add(this.strMod);
             this.Controls.Add(this.RaceLabel);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.otherInfoLbl);
             this.Controls.Add(this.HPLabel);
             this.Controls.Add(this.ACLabel);
-            this.Controls.Add(this.ChaLabel);
-            this.Controls.Add(this.IntLabel);
-            this.Controls.Add(this.WisLabel);
-            this.Controls.Add(this.ConsLabel);
-            this.Controls.Add(this.DexLabel);
             this.Controls.Add(this.HitDiceBox);
             this.Controls.Add(this.label11);
-            this.Controls.Add(this.StrLabel);
             this.Controls.Add(this.ClassLabel);
             this.Controls.Add(this.numericUpDown8);
             this.Controls.Add(this.label10);
@@ -409,18 +475,24 @@
         private System.Windows.Forms.NumericUpDown numericUpDown8;
         private System.Windows.Forms.Label label10;
         private System.Windows.Forms.Label ClassLabel;
-        private System.Windows.Forms.Label StrLabel;
         private System.Windows.Forms.Label label11;
         private System.Windows.Forms.TextBox HitDiceBox;
-        private System.Windows.Forms.Label DexLabel;
-        private System.Windows.Forms.Label ConsLabel;
-        private System.Windows.Forms.Label WisLabel;
-        private System.Windows.Forms.Label IntLabel;
-        private System.Windows.Forms.Label ChaLabel;
         private System.Windows.Forms.Label ACLabel;
         private System.Windows.Forms.Label HPLabel;
         private System.Windows.Forms.TextBox otherInfoLbl;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label RaceLabel;
+        private System.Windows.Forms.Label strMod;
+        private System.Windows.Forms.Label dexMod;
+        private System.Windows.Forms.Label conMod;
+        private System.Windows.Forms.Label wisMod;
+        private System.Windows.Forms.Label intMod;
+        private System.Windows.Forms.Label chaMod;
+        private System.Windows.Forms.Label StrLabel;
+        private System.Windows.Forms.Label DexLabel;
+        private System.Windows.Forms.Label ConLabel;
+        private System.Windows.Forms.Label WisLabel;
+        private System.Windows.Forms.Label IntLabel;
+        private System.Windows.Forms.Label ChaLabel;
     }
 }
--- DnDCharacterGen/BasicCreateForm.cs
+++ DnDCharacterGen/BasicCreateForm.cs
@@ -49,6 +49,8 @@ namespace DnDCharacterGen
                     "bludgeoning, piercing and slashing damage. When you attack with STR you gain bonus to " +
                     "damage roll as per barbarian table.";
             }
+            
+
         }
 
         private void BasicCreateForm_Load(object sender, EventArgs e)
@@ -59,6 +61,46 @@ namespace DnDCharacterGen
         private void button1_Click(object sender, EventArgs e)
         {
             Close();
+        }
+
+        public void changeMod(Label label, NumericUpDown numericMod) 
+        {
+            label.ResetText();
+            int strMod = Convert.ToInt32(numericMod.Value);
+            if (strMod > 20) { numericMod.Value = 20; strMod = 20; }
+            int modifier;
+            if (strMod > 10) { modifier = (strMod - 10) / 2; } else { modifier = 0; }
+            label.Text = ("+"+modifier.ToString());
+        }
+
+        private void strMod_Click(object sender, EventArgs e)
+        {
+            changeMod(strMod, numericUpDown1);
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            changeMod(dexMod, numericUpDown2);
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+            changeMod(conMod, numericUpDown3);
+        }
+
+        private void numericUpDown4_ValueChanged(object sender, EventArgs e)
+        {
+            changeMod(wisMod, numericUpDown4);
+        }
+
+        private void numericUpDown5_ValueChanged(object sender, EventArgs e)
+        {
+            changeMod(intMod, numericUpDown5);
+        }
+
+        private void numericUpDown6_ValueChanged(object sender, EventArgs e)
+        {
+            changeMod(chaMod, numericUpDown6);
         }
     }
 }
